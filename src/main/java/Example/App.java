f349feb20006ceef2212e64d20001a46a008730c
manugdf <manugdf94@gmail.com>
2017-01-13 03:10:44 +0100

    Commit inicial

---
+++ src/main/java/Example/App.java
@@ -0,0 +1,13 @@
+package Example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( " La cosa más difícil es conocernos a nosotros mismos; la más fácil es hablar mal de los demás" );
+    }
+}
